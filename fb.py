90d5018b0c52466ebf39e1bca3138267cc20f8f2
ycliang-tw <ycliang@cs.nctu.edu.tw>
2019-09-18 14:28:52 +0800

    Add PTT and FB crawler for keyword "VAPE"

--- fb.py
+++ fb.py
@@ -1,21 +1,68 @@
 from bs4 import BeautifulSoup
+from bs4 import Comment
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 import requests
+import time
+import csv
 
-options = Options()
-options.headless = True
-driver = webdriver.Firefox(options=options)
+def writecsv(keyword, list_of_dictionaries):
+	with open("fb_vape.csv", "a", newline='') as csvfile:
+		write = csv.writer(csvfile, delimiter=',')
+		write.writerow(['關鍵字', '頁面名稱', '連結網址'])
+		for entry in list_of_dictionaries:
+			write.writerow([keyword, entry['title'], entry['link']])
+		
 
-url = "https://facebook.com/public?query=電子菸&type=pages&init=dir&nomc=0"
+def scrape_fb_fanpage(keyword):
+	url = "https://facebook.com/public?query=" + keyword + "&type=pages&init=dir&nomc=0"
+	# use selenium to simulate user behavior
+	# open firefox with no display mode (or else will produce error in my current evironment)
+	# reference: https://stackoverflow.com/questions/52534658/webdriverexception-message-invalid-argument-cant-kill-an-exited-process-with (2nd answer)
+	options = Options()
+	options.headless = True
+	driver = webdriver.Firefox(options=options)	# open firefox
+	driver.get(url)
+	last_height = driver.execute_script("return document.body.scrollHeight")	# get current window height
+	# scroll till the bottom has been reached
+	
+	pause_time = 3
+	while True:
+		driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+		time.sleep(pause_time)
+		new_height = driver.execute_script("return document.body.scrollHeight")
+		print(new_height)
+		if new_height == last_height:
+			break
+		last_height = new_height
+	
+	data = driver.page_source
+	# parse source html
+	soup = BeautifulSoup(data, "html.parser")
+	fanpages = soup.find_all(class_="_32mo")
+	content = []
+	for entry in fanpages:
+		info = {
+			'link' : entry['href'],
+			'title': entry.text
+		}
+		print(info)
+		content.append(info)
+	return content
 
-r1 = requests.Session()
-r = r1.get(url)
-#print(r.text)
 
-soup = BeautifulSoup(r.text, "html.parser")
-#print(soup)
-hidden = soup.find_all(class_="hidden_elem")
-#print(hidden)
-code = soup.find_all("code", id="u_0_f")
-print(code)
+
+def main():
+	search_list = {	
+		"電子菸",
+		"電子煙",
+		"vape"
+	}
+	data = {}
+	for key_word in search_list:
+		data = scrape_fb_fanpage(key_word)
+		writecsv(key_word, data)
+	
+
+if __name__ == '__main__':
+	main()
--- ptt_crawler.py
+++ ptt_crawler.py
@@ -2,14 +2,12 @@
 # import urllib.request as req
 from bs4 import BeautifulSoup
 import requests
-from openpyxl import Workbook
+# from openpyxl import Workbook 
+# cannot use openpyxl because if exception occurs
+# the workbook will not be written or may only be written with small amount of data
+import csv
 
-wb = Workbook()
-ws = wb.active
-ws['A1'] = "標題"
-ws['B1'] = "作者"
-ws['C1'] = "內文"
-ws['D1'] = "連結"
+
 
 ## set up requests
 url = "https://www.ptt.cc/bbs/VAPE/index.html"
@@ -21,31 +19,34 @@ payload = {	"from": "/bbs/VAPE/index.html", "yes": "yes"}
 r = requests.Session()
 r1 = r.post(url2, payload)
 
-for i in range(100):
-	r2 = r.get(url, headers=headers)
-	soup = BeautifulSoup(r2.text, "html.parser")
-	r_ent = soup.find_all(class_="r-ent")
-	pre = soup.select("div.btn-group.btn-group-paging a")
-	url = "https://www.ptt.cc" + pre[1]["href"]
-	for entry in r_ent:
-		title = entry.find(class_="title").text.strip()
-		if "買賣" in title:
-			url2 = "https://www.ptt.cc" + entry.find('a')['href']
-			author = entry.find(class_="author").text.strip()
-			r3 = r.get(url2)
-			soup2 = BeautifulSoup(r3.text, "html.parser")
-			content = soup2.find(id="main-content").text
-			target = '※ 發信站: 批踢踢實業坊(ptt.cc),'
-			content = content.split(target)
-			try:
-				time = soup2.select(".article-metaline")[2].text
-				content = content[0].split(time)
-			except Exception as e:
-				print(e)
-			print(url2)
-			print(author)
-			print(title)
-			print(content[1])
-			ws.append([title, author, content[1], url2])
+with open('VAPE.csv', 'w', newline='') as csvfile:
+	write = csv.writer(csvfile, delimiter=',')
+	write.writerow(['標題', 'ID', '內文', '連結'])
+	for i in range(1):
+		r2 = r.get(url, headers=headers)
+		soup = BeautifulSoup(r2.text, "html.parser")
+		r_ent = soup.find_all(class_="r-ent")
+		pre = soup.select("div.btn-group.btn-group-paging a")
+		url = "https://www.ptt.cc" + pre[1]["href"]
+		for entry in r_ent:
+			title = entry.find(class_="title").text.strip()
+			if "買賣" in title:
+				url2 = "https://www.ptt.cc" + entry.find('a')['href']
+				author = entry.find(class_="author").text.strip()
+				r3 = r.get(url2)
+				soup2 = BeautifulSoup(r3.text, "html.parser")
+				content = soup2.find(id="main-content").text
+				target = '※ 發信站: 批踢踢實業坊(ptt.cc),'
+				content = content.split(target)
+				try:
+					time = soup2.select(".article-metaline")[2].text
+					content = content[0].split(time)
+				except Exception as e:
+					print(e)
+				print(url2)
+				print(author)
+				print(title)
+				print(content[1])
+				write.writerow([title, author, content[1], url2])
+	
 
-wb.save("PTT_VAPE.xlsx")
